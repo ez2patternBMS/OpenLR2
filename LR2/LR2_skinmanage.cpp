ez2patternBMS/OpenLR2

commit 60d3828958b7c692d1ae30ae57ba3c388548a935

refactor: use std::filesystem more

LR2/LR2_skinmanage.cpp
@@ -2,6 +2,7 @@
 #include "Engine.h"
 #include "LR2_configsave.h"
 
+#include <filesystem>
 
 //maybe deleted by compiler, restored it for convenience
 int SetFirstSkin(SkinManage *sm, SKINTYPE type, CSTR *skinName) {
@@ -325,34 +326,21 @@ int ParseLR2SkinCustom(SkinManage *skm, CSTR filepath) {
 
 //4a8230 MakeSkinList
 int MakeSkinList(SkinManage *skm, CSTR dir) {
-	HANDLE hFindFile, hFindFileOld;
-	_WIN32_FIND_DATAA findFileData;
-	CSTR filter;
-	bool isLR2Skin;
-	if (dir.right(1).isDiff("\\")) 
+	if (dir.right(1).isDiff("\\") && dir.right(1).isDiff("/"))
 		dir.add("\\");
-	
-	filter.assign(&dir).add("*");
-	hFindFile = FindFirstFileA(filter, &findFileData);
-	do{
-		if ( (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0) {
-			filter.assign(&dir).add(findFileData.cFileName);
-			isLR2Skin = false;
-			if (filter.right(8).isSame(".lr2skin"))		isLR2Skin = true;
-			else if (filter.right(6).isSame(".lr2ss"))	isLR2Skin = true;
-			
-			if (isLR2Skin) ParseLR2SkinCustom(skm, filter); 
-		}
-		else {  //logic arranged
-		if (strcmp("..", findFileData.cFileName) && strcmp(".", findFileData.cFileName)) {
-			filter.assign(&dir).add(findFileData.cFileName).add("\\");
+#ifndef _WIN32
+	dir.replace("\\", "/");
+#endif // _WIN32
+	CSTR filter;
+	for (const auto& entry : std::filesystem::directory_iterator(dir.body)) {
+		if (entry.is_directory()) {
+			filter.assign(&dir).add(entry.path().filename().string().c_str());
 			MakeSkinList(skm, filter);
+		} else {
+			filter.assign(&dir).add(entry.path().filename().string().c_str());
+			if (filter.right(8).isSame(".lr2skin") || filter.right(6).isSame(".lr2ss"))
+				ParseLR2SkinCustom(skm, filter);
 		}
-		}
-		hFindFileOld = hFindFile;
-		if (FindNextFileA(hFindFile, &findFileData) == 0) {
-			FindClose(hFindFileOld);
-			return 0;
-		}
-	} while (true);
+	}
+	return 0;
 }

LR2/Main.cpp
@@ -6,6 +6,7 @@
 #include "Scenes.h"
 
 #include <windows.h>
+#include <filesystem>
 #include <string>
 #include <thread>
 
@@ -40,22 +41,28 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	int lr1ir;
 	int lr2ir;
 	game gs;
-	char curDir[260];
 
 	const bool use_dx9 = getenv("OPENLR2_NO_DX9") == nullptr; // chown2: crashes on DxLib_Init with DX9 for me
 
 	int tmp;
-	
+
 	//start of code
+	char curDir[260];
 	GetModuleFileName(NULL, (LPCH)curDir, 260);
 	*(char*)(strrchr(curDir, '\\') + 1) = '\x00';
 	SetCurrentDirectory((LPCSTR)curDir);
 	gs.baseDirectory.assign(curDir, 0).add("\\");
 	gs.is_starter = false;
-	CopyFileA("LR2files\\Config\\keyconfig_def.xml", "LR2files\\Config\\keyconfig.xml", 1);
-	CopyFileA("LR2files\\Config\\keyconfig_5_def.xml", "LR2files\\Config\\keyconfig_5.xml", 1);
-	CopyFileA("LR2files\\Config\\keyconfig_p_def.xml", "LR2files\\Config\\keyconfig_p.xml", 1);
-	CopyFileA("LR2files\\Config\\midi_def.xml", "LR2files\\Config\\midi.xml", 1);
+	auto copy_if_not_exists = [](auto&& from, auto&& to_) {
+		std::filesystem::path to = to_;
+		std::error_code ec; // ignore errors
+		if (!std::filesystem::exists(to, ec))
+			std::filesystem::copy(from, to, ec);
+	};
+	copy_if_not_exists("LR2files/Config/keyconfig_def.xml", "LR2files/Config/keyconfig.xml");
+	copy_if_not_exists("LR2files/Config/keyconfig_5_def.xml", "LR2files/Config/keyconfig_5.xml");
+	copy_if_not_exists("LR2files/Config/keyconfig_p_def.xml", "LR2files/Config/keyconfig_p.xml");
+	copy_if_not_exists("LR2files/Config/midi_def.xml", "LR2files/Config/midi.xml");
 	ErrorLogAdd("コンフィグを読み込みます…");
 	
 	if (!ReadConfig(&gs, "LR2files\\Config\\config.xml") && gs.is_starter == false) {
@@ -189,12 +196,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		if (gs.config.play.bga == 3) gs.config.play.bga = 1;
 		if (gs.config.select.disabledifficultyfilter == 1) gs.config.select.ignoredifficultyall = 0;
 		memcpy(&gs.sSelect.filter, &gs.config.select, sizeof(CONFIG_SELECT));
-		cstrSprintf(&newPath, "LR2files/Replay/%s", gs.config.player.id);
-		CreateDirectoryA(newPath, NULL);
-		cstrSprintf(&newPath, "LR2files/Ghost/%s", gs.config.player.id);
-		CreateDirectoryA(newPath, NULL);
-		CreateDirectoryA("LR2files/SkinCustomize", NULL);
-		CreateDirectoryA("screenshot", NULL);
+		{
+			std::error_code ec; // ignore errors
+			cstrSprintf(&newPath, "LR2files/Replay/%s", gs.config.player.id.body);
+			std::filesystem::create_directories(newPath.body, ec);
+			cstrSprintf(&newPath, "LR2files/Ghost/%s", gs.config.player.id.body);
+			std::filesystem::create_directories(newPath.body, ec);
+			std::filesystem::create_directories("LR2files/SkinCustomize", ec);
+			std::filesystem::create_directories("screenshot", ec);
+		}
 		ReadKeyConfig(&gs, "LR2files\\Config\\keyconfig.xml");
 		gs.is_clicked_screenModeChange = 0;
 		gs.flag_Screenshot = 0;
@@ -1544,7 +1554,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 											if ( abs(gs.skstruct.dst_JUDGELINE[1].draw->w - gs.skstruct.image.dst[i].draw[gs.skstruct.image.dst[i].dstCount - 1].w) <= 10.0 
 												&& abs(gs.skstruct.dst_JUDGELINE[1].draw->x - gs.skstruct.image.dst[i].draw[gs.skstruct.image.dst[i].dstCount - 1].x) <= 5.0
 												&& (gs.skstruct.dst_JUDGELINE[1].draw->y >= gs.skstruct.image.dst[i].draw[gs.skstruct.image.dst[i].dstCount - 1].y || gs.skstruct.image.dst[i].draw[gs.skstruct.image.dst[i].dstCount - 1].h < 0.0)) {
-													
+
 												objx = gs.skstruct.adjust.note_2p_x;
 												objy = gs.skstruct.adjust.note_2p_y;
 											}
@@ -1552,7 +1562,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 									}
 								}
 							}
-							
+
 							if (gs.skstruct.image.dst[i].opt4 == 1) {
 								AddDrawingBuffer_Scratch(&gs.skstruct.drBuf, &gs.skstruct.image.src[i], &gs.skstruct.image.dst[i], &gs.timer1, gs.skstruct.scratchAngle_1);
 							}
@@ -1588,7 +1598,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 							}
 						}
 					}
-					
+
 				}
 
 				if (gs.procSelecter == 4 || gs.is_starter) {
@@ -1842,7 +1852,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 							}
 							else if (gs.KeyInput.inputID[KEY_INPUT_DOWN] == 1 && gs.config.select.disabledifficultyfilter == 0) {
 								gs.sSelect.bmsList[gs.sSelect.cur_song].difficulty++;
-								
+
 								if (gs.sSelect.bmsList[gs.sSelect.cur_song].difficulty >= 6)
 									gs.sSelect.bmsList[gs.sSelect.cur_song].difficulty = 1;
 								else if (gs.sSelect.bmsList[gs.sSelect.cur_song].difficulty < 1)

commit 89aec1e6a9ee785b9ecebe3d61fad97c091a6afb

fix: undo std::filesystem::path usage
Fixes game crashing if there are non-ASCII symbols in game path or in
skin filenames.

Fixes: 60d3828958b7 ("refactor: use std::filesystem more")

LR2/LR2_skinmanage.cpp
@@ -383,6 +383,30 @@ int ParseLR2SkinCustom(SkinManage *skm, CSTR filepath) {
 int MakeSkinList(SkinManage *skm, CSTR dir) {
 	if (dir.right(1).isDiff("\\") && dir.right(1).isDiff("/"))
 		dir.add("\\");
+#ifdef _WIN32
+	CSTR filter;
+	filter.assign(&dir).add("*");
+	HANDLE hFindFile, hFindFileOld;
+	_WIN32_FIND_DATAA findFileData;
+	hFindFile = FindFirstFileA(filter, &findFileData);
+	do{
+		if ( (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0) {
+			filter.assign(&dir).add(findFileData.cFileName);
+			if (filter.right(8).isSame(".lr2skin") || filter.right(6).isSame(".lr2ss")) ParseLR2SkinCustom(skm, filter);
+		}
+		else {  //logic arranged
+		if (strcmp("..", findFileData.cFileName) && strcmp(".", findFileData.cFileName)) {
+			filter.assign(&dir).add(findFileData.cFileName).add("\\");
+			MakeSkinList(skm, filter);
+		}
+		}
+		hFindFileOld = hFindFile;
+		if (FindNextFileA(hFindFile, &findFileData) == 0) {
+			FindClose(hFindFileOld);
+			return 0;
+		}
+	} while (true);
+#else // TODO(utf-8): use this
 #ifndef _WIN32
 	dir.replace("\\", "/");
 #endif // _WIN32
@@ -398,4 +422,5 @@ int MakeSkinList(SkinManage *skm, CSTR dir) {
 		}
 	}
 	return 0;
+#endif // _WIN32
 }

LR2/Main.cpp
@@ -5,8 +5,8 @@
 #include "LR2.h"
 #include "Scenes.h"
 
+#include <chrono>
 #include <filesystem>
-#include <string>
 #include <thread>
 
 #include <DxLib/DxLib.h>
@@ -27,13 +27,15 @@ int WINAPI WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPSTR /
 	return main(__argc, __argv);
 }
 
+/* TODO(utf-8): use this
 static std::filesystem::path GetExecutablePath()
 {
-	char fullpath[256] = { 0 };
-	if (!GetModuleFileNameA(nullptr, fullpath, sizeof(fullpath)))
+	wchar_t fullpath[256]{};
+	if (!GetModuleFileNameW(nullptr, fullpath, std::size(fullpath)))
 		return {};
 	return std::filesystem::path(fullpath).parent_path();
 }
+*/
 
 #else
 
@@ -46,7 +48,7 @@ static void MessageBoxA(const char*, const char* title, const char* desc, const 
 
 static std::filesystem::path GetExecutablePath()
 {
-	char fullpath[256] = { 0 };
+	char fullpath[256]{};
 
 	char process_path[] = "/proc/self/exe";
 	const auto bytes =
@@ -93,9 +95,22 @@ int main(int argc, char** argv) {
 
 	int tmp;
 
-	auto curDir = GetExecutablePath();
-	std::filesystem::current_path(curDir);
-	gs.baseDirectory.assign(curDir.string().c_str(), 0).add("/");
+#ifdef _WIN32
+	{
+		char curDir[260];
+		GetModuleFileName(NULL, (LPCH)curDir, 260);
+		*(char*)(strrchr(curDir, '\\') + 1) = '\x00';
+		SetCurrentDirectory((LPCSTR)curDir);
+		gs.baseDirectory.assign(curDir, 0).add("/");
+	}
+#else // TODO(utf-8): use this
+	{
+		auto curDir = GetExecutablePath();
+		std::filesystem::current_path(curDir);
+		gs.baseDirectory.assign(curDir.string().c_str(), 0).add("/");
+	}
+#endif // _WIN32
+
 	gs.is_starter = false;
 	auto copy_if_not_exists = [](auto&& from, auto&& to_) {
 		std::filesystem::path to = to_;