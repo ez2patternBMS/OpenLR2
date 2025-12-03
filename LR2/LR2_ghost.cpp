ez2patternBMS/OpenLR2

commit 43b628d2eb57fa2b261a2ebb3e1203a6aed37722

refactor: simplify and deduplicate ghost DB reading/writing
Don't format queries by hand while at it.

LR2/LR2_ghost.cpp
@@ -787,29 +787,38 @@ int WriteGhostInDatabase(sqlite3 *sql, CSTR songMD5, PLAYSCORE *score) {
 	ErrorLogAdd("データベースにゴーストを書き込みます\n");
 
 	CSTR ghostdata = score->EncodeGhostData();
-	CSTR query;
-	cstrSprintf(&query, "UPDATE score SET ghost = \'%s\' WHERE hash = \'%s\'", ghostdata.body, songMD5.body);
-	SQL_Run(query, sql);
+
+	sqlite3_stmt *pStmt;
+	sqlite3_prepare_v3(sql, "UPDATE score SET ghost = ? WHERE hash = ?", 0, 0, &pStmt, nullptr);
+	sqlite3_bind_text(pStmt, 1, ghostdata.body, ghostdata.length(), nullptr);
+	sqlite3_bind_text(pStmt, 2, songMD5.body, songMD5.length(), nullptr);
+	sqlite3_step(pStmt);
+	sqlite3_finalize(pStmt);
+
 	ErrorLogAdd("ゴーストの書き込みが終了しました\n");
 	return 0;
 }
 
-//4449f0
-int ReadGhostToScore(sqlite3 *sql, CSTR songMD5, PLAYSCORE *score) {
-
-	char query[1024];
-	sqlite3_stmt *pStmt;
-	CSTR ghostdata;
-
+CSTR ReadGhost(sqlite3 *sql, CSTR songMD5) {
 	ErrorLogAdd("データベースからゴーストを読み込みます\n");
 
-	sqlite3_snprintf(0x400, query, "SELECT ghost FROM score WHERE hash = \'%q\'", songMD5.body);
-	SQL_prepare(query, sql, &pStmt);
+	sqlite3_stmt *pStmt;
+	sqlite3_prepare_v3(sql, "SELECT ghost FROM score WHERE hash = ?", 0, 0, &pStmt, nullptr);
+	sqlite3_bind_text(pStmt, 1, songMD5.body, songMD5.length(), nullptr);
 
-	if (sqlite3_step(pStmt) == 100) {
-		ghostdata = SQL_GetColumn(0, pStmt);
+	CSTR ghostdata;
+	if (sqlite3_step(pStmt) == SQLITE_ROW) {
+		ghostdata.resize(sqlite3_column_bytes(pStmt, 1));
+		// Cast safety: it's safe to cast from unsigned char* to char*
+		ghostdata = reinterpret_cast<const char*>(sqlite3_column_text(pStmt, 1));
 	}
 	sqlite3_finalize(pStmt);
+
+	return ghostdata;
+}
+
+int ReadGhostToScore(sqlite3 *sql, CSTR songMD5, PLAYSCORE *score) {
+	CSTR ghostdata = ReadGhost(sql, songMD5);
 
 	if (ghostdata.length() == 0) {
 		score->InitJudgeQueue();
@@ -818,24 +827,4 @@ int ReadGhostToScore(sqlite3 *sql, CSTR songMD5, PLAYSCORE *score) {
 	
 	score->DecodeGhostData(ghostdata);
 	return 1;
-}
-
-//444b80
-CSTR ReadGhost(sqlite3 *sql, CSTR songMD5) {
-
-	char query[1024];
-	sqlite3_stmt *pStmt;
-	CSTR ghostdata;
-
-	ErrorLogAdd("データベースからゴーストを読み込みます\n");
-
-	sqlite3_snprintf(0x400, query, "SELECT ghost FROM score WHERE hash = \'%q\'", songMD5.body);
-	SQL_prepare(query, sql, &pStmt);
-
-	if (sqlite3_step(pStmt) == 100) {
-		ghostdata = SQL_GetColumn(0, pStmt);
-	}
-	sqlite3_finalize(pStmt);
-
-	return ghostdata;
 }

commit bcb27847149690f6184253caaeb25b56405a22cd

fix: fix ghost reading/writing indexes and string lengths (regression)
John Bacon issue.
sqlite3_prepare_v3 takes -1 instead of 0 to strlen() it's input string.
sqlite3_column_* count starting from 0 (unlike sqlite3_bind_*).

Fixes: 43b628d2eb57 ("refactor: simplify and deduplicate ghost DB reading/writing")

LR2/LR2_ghost.cpp
@@ -731,11 +731,21 @@ int WriteGhostInDatabase(sqlite3 *sql, CSTR songMD5, PLAYSCORE *score) {
 	CSTR ghostdata = score->EncodeGhostData();
 
 	sqlite3_stmt *pStmt;
-	sqlite3_prepare_v3(sql, "UPDATE score SET ghost = ? WHERE hash = ?", 0, 0, &pStmt, nullptr);
-	sqlite3_bind_text(pStmt, 1, ghostdata.body, ghostdata.length(), nullptr);
-	sqlite3_bind_text(pStmt, 2, songMD5.body, songMD5.length(), nullptr);
-	sqlite3_step(pStmt);
-	sqlite3_finalize(pStmt);
+	if (sqlite3_prepare_v3(sql, "UPDATE score SET ghost = ? WHERE hash = ?", -1, 0, &pStmt, nullptr) != SQLITE_OK) {
+		ErrorLogAdd("sqlite3_prepare_v3 error\n");
+	}
+	if (sqlite3_bind_text(pStmt, 1, ghostdata.body, ghostdata.length(), nullptr) != SQLITE_OK) {
+		ErrorLogAdd("sqlite3_bind_text error\n");
+	}
+	if (sqlite3_bind_text(pStmt, 2, songMD5.body, songMD5.length(), nullptr) != SQLITE_OK) {
+		ErrorLogAdd("sqlite3_bind_text error\n");
+	}
+	if (sqlite3_step(pStmt) != SQLITE_OK) {
+		ErrorLogAdd("sqlite3_step error\n");
+	}
+	if (sqlite3_finalize(pStmt) != SQLITE_OK) {
+		ErrorLogAdd("sqlite3_finalize error\n");
+	}
 
 	ErrorLogAdd("ゴーストの書き込みが終了しました\n");
 	return 0;
@@ -745,16 +755,22 @@ CSTR ReadGhost(sqlite3 *sql, CSTR songMD5) {
 	ErrorLogAdd("データベースからゴーストを読み込みます\n");
 
 	sqlite3_stmt *pStmt;
-	sqlite3_prepare_v3(sql, "SELECT ghost FROM score WHERE hash = ?", 0, 0, &pStmt, nullptr);
-	sqlite3_bind_text(pStmt, 1, songMD5.body, songMD5.length(), nullptr);
+	if (sqlite3_prepare_v3(sql, "SELECT ghost FROM score WHERE hash = ?", -1, 0, &pStmt, nullptr) != SQLITE_OK) {
+		ErrorLogAdd("sqlite3_prepare_v3 error\n");
+	}
+	if (sqlite3_bind_text(pStmt, 1, songMD5.body, songMD5.length(), nullptr) != SQLITE_OK) {
+		ErrorLogAdd("sqlite3_bind_text error\n");
+	}
 
 	CSTR ghostdata;
 	if (sqlite3_step(pStmt) == SQLITE_ROW) {
-		ghostdata.resize(sqlite3_column_bytes(pStmt, 1));
+		ghostdata.resize(sqlite3_column_bytes(pStmt, 0));
 		// Cast safety: it's safe to cast from unsigned char* to char*
-		ghostdata = reinterpret_cast<const char*>(sqlite3_column_text(pStmt, 1));
+		ghostdata = reinterpret_cast<const char*>(sqlite3_column_text(pStmt, 0));
 	}
-	sqlite3_finalize(pStmt);
+	if (sqlite3_finalize(pStmt) != SQLITE_OK) {
+		ErrorLogAdd("sqlite3_finalize error\n");
+	}
 
 	return ghostdata;
 }