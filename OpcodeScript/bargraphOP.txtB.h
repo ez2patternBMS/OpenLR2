7a1c748fe4dee59e2c5803d781d1027c7220aa3c
GOMazk <gommazuk@naver.com>
2025-12-24 14:49:30 +0900

    added opcode list for bargraph

---
+++ OpcodeScript/bargraphOP.txtB.h
@@ -0,0 +1,45 @@
+case 1: return "songProgress";
+case 2: return "songLoaded";
+
+case 3: return "level_bar";
+
+case 5: return "samefolder_beginner_level";
+case 6: return "samefolder_normal_level";
+case 7: return "samefolder_hyper_level";
+case 8: return "samefolder_anotherl_level";
+case 9: return "samefolder_insane_level";
+
+case 10: return "1P_EXSCORE_now";
+case 11: return "1P_EXSCORE_predict";
+case 12: return "1P_HIGHSCORE_now";
+case 13: return "1P_HIGHSCORE_predict";
+case 14: return "2P_target_EXSCORE_now";
+case 15: return "2P_target_EXSCORE_predict";
+
+case 20: return "1P_PG_totalnotes";
+case 21: return "1P_GR_totalnotes";
+case 22: return "1P_GD_totalnotes";
+case 23: return "1P_BD_totalnotes";
+case 24: return "1P_PR_totalnotes";
+case 25: return "1P_MAXCOMBO_totalnotes";
+case 26: return "1P_SCORE_200000";
+case 27: return "1P_EXSCORE_totalnotes__2";
+
+
+case 30: return "2P_target_PG_totalnotes";
+case 31: return "2P_target_GR_totalnotes";
+case 32: return "2P_target_GD_totalnotes";
+case 33: return "2P_target_BD_totalnotes";
+case 34: return "2P_target_PR_totalnotes";
+case 35: return "2P_target_MAXCOMBO_totalnotes";
+case 36: return "2P_target_SCORE_200000";
+case 37: return "2P_target_exscore_totalnotes__2";
+
+case 40: return "HIGHSCORE_PG_totalnotes";
+case 41: return "HIGHSCORE_GR_totalnotes";
+case 42: return "HIGHSCORE_GD_totalnotes";
+case 43: return "HIGHSCORE_BD_totalnotes";
+case 44: return "HIGHSCORE_PR_totalnotes";
+case 45: return "HIGHSCORE_MAXCOMBO_totalnotes";
+case 46: return "HIGHSCORE_SCORE_200000";
+case 47: return "HIGHSCORE_EXSCORE_totalnotes__";
