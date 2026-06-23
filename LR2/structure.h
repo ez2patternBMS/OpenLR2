ez2patternBMS/OpenLR2

commit 9e14582489c888e9324133e381ff982f0c043338

feat: add LR2OOL SRC_NUMBER 295 418 - random layout numbers
Pretty sure LR2OOL implements this incorrectly for key modes other than
7. This implementation only shows 'keymode' numbers instead.
CC @tenaibms

LR2/structure.h
@@ -1319,6 +1319,7 @@ struct gameplay {
 	int fadeoutBGAend[10];
 	int bgaMixer[10];
 	char isPreviewLoad;
+	int randomLayoutForDisplay[2];
 	std::future<void> hThreadPreview;
 	int previewStatus; /* 1:start 2:loaded */
 	CSTR previewBMShash;

LR2/LR2_bmsload.cpp
@@ -1997,6 +1997,10 @@ int ParseBmsFile(gameplay *gp, CSTR filename, AUDIO *aud, ConfigStruct* cfg, BMS
 	}
 	endtime = 0.0;
 
+	int key = 7;
+	if (meta->keymode == 5 || meta->keymode == 10) key = 5;
+	else if (meta->keymode == 9) key = 9;
+
 	//TOFIX : in nonstop mode(courseType==1), gp->courseConnection[stage - 1] doesn't check if stage >= 1. It can affects at #BPM
 	/* start of stage loop */
 	for (int stage = 0; stage < stages; stage++) {
@@ -2881,9 +2885,6 @@ int ParseBmsFile(gameplay *gp, CSTR filename, AUDIO *aud, ConfigStruct* cfg, BMS
 			double t_bmsTiming = 0.0;
 			memset(mapAdded, 0, 20);
 			int addNoteCount[2] = { 0, };
-			int key = 7;
-			if (meta->keymode == 5 || meta->keymode == 10) key = 5;
-			else if (meta->keymode == 9) key = 9;
 
 			for (int i = 0; i < gp->bmsobj.count; i++) {
 				if (l_realTiming < gp->bmsobj.notes[i].realTiming) {
@@ -2959,9 +2960,6 @@ int ParseBmsFile(gameplay *gp, CSTR filename, AUDIO *aud, ConfigStruct* cfg, BMS
 			double t_bmsTiming = 0.0;
 			bool mapAdded[2][10] = { 0, };
 			int addNoteCount[2] = { 0, };
-			int key = 7;
-			if (meta->keymode == 5 || meta->keymode == 10) key = 5;
-			else if (meta->keymode == 9) key = 9;
 
 			for (int i = 0; i < gp->bmsobj.count; i++) {
 				if (l_realTiming < gp->bmsobj.notes[i].realTiming) {
@@ -3367,6 +3365,11 @@ int ParseBmsFile(gameplay *gp, CSTR filename, AUDIO *aud, ConfigStruct* cfg, BMS
 				}
 			}
 		}
+	}
+
+	for (int p : {0, 1}) {
+		gp->randomLayoutForDisplay[p] = 0;
+		for (int i = 1; i < 1 + key; ++i) gp->randomLayoutForDisplay[p] += std::pow(10, p * 10 + key - noteRandomTable[p][i]) * i;
 	}
 
 	double p1LastTiming = 0.0, p2LastTiming = 0.0;

LR2/LR2_skinobject.cpp
@@ -2211,10 +2211,11 @@ uint SetObjectValue_Num(game *g, int op) {
 			//291 is same 91
 		case 292:
 			return g->sSelect.bmsList[g->sSelect.cur_song].rivalRecord.IRranking;
-			break;
 			//293 is same 180
 		case 294: //TOFIX : IR clear rate
 			break;
+		case 295: return g->gameplay.randomLayoutForDisplay[0]; // LR2OOL SP and DP 1P random
+		case 418: return g->gameplay.randomLayoutForDisplay[1]; // LR2OOL DP 2P random
 	}
 	return 0;
 }

commit dfae4205214458d38c0256ec81b8241289a04926

fix(structure): init randomLayoutForDisplay (1/2)
Fixes: 9e14582489c8 ("feat: add LR2OOL SRC_NUMBER 295 418 - random layout numbers")

LR2/structure.h
@@ -1331,7 +1331,7 @@ struct gameplay {
 	int fadeoutBGAend[10];
 	int bgaMixer[10];
 	char isPreviewLoad;
-	int randomLayoutForDisplay[2];
+	int randomLayoutForDisplay[2]{};
 	unsigned int forceRandomLayout; // 0 = disabled
 	std::future<void> hThreadPreview;
 	int previewStatus; /* 1:start 2:loaded */